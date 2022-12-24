d50197bbb9c0ce49c9337572d536abc80ba3b609
reemydiv <119451544+reemydiv@users.noreply.github.com>
2022-12-24 17:16:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function turnOn(){
+
+document.getElementById('image').src='https://images.yaoota.com/uwXAyVn2JOQtCPjlui_VhBEngAw=/trim/fit-in/500x500/filters:quality(80)/yaootaweb-production/media/crawledproductimages/8dfe6408625593636e541293ed645edf19a8b7a1.jpg';
+}
+function turnOff(){
+    document.getElementById('image').src='https://m.media-amazon.com/images/I/512d9OFHCXL._AC_SY500_.jpg';
+}
